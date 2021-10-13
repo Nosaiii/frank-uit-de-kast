56577856dc3e1ffea37a4ec9b57e9150265fbd31
Pepijn <pepijnvdliefvoort@live.nl>
2021-10-13 12:33:14 +0200

    Renamed class

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 
 [RequireComponent(typeof(Rigidbody))]
-public class CameraMovement : MonoBehaviour
+public class PlayerMovement : MonoBehaviour
 {
     [Header("Object references")]
     [SerializeField]
